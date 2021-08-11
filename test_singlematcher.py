15551c2921e931b98508b1e92826958d2bab9673
Abdullah Ariff <abdullah.ariff@made.com>
2021-08-11 22:46:28 +0100

    Test docstring

--- test_singlematcher.py
+++ test_singlematcher.py
@@ -1,3 +1,4 @@
+import inspect
 from .singlematcher import singlematcher
 
 def test_calling_decorated_function_returns_default():
@@ -31,4 +32,21 @@ def test_matching_a_registered_matcher():
   
   assert do_thing("foo") == "default match"
   assert do_thing("matchme!") == "I match!"
+
+
+def test_always_uses_default_matcher_docstring():
+  @singlematcher
+  def do_thing(match):
+    """
+    This is my docstring.
+    """
+    return "default match"
+
+  @do_thing.register("matchme!")
+  def _(match):
+    "This is some other docstring"
+    return "I match!" 
+
+  assert inspect.getdoc(do_thing) == "This is my docstring."
+
   
