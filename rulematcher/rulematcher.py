414a118f71f23d0b77bd1e86fae837d89a709b6a
Abdullah Ariff <24273084+abdullahariff@users.noreply.github.com>
2021-09-11 21:37:14 +0100

    Implement (ugly) strategy registration

--- rulematcher/rulematcher.py
+++ rulematcher/rulematcher.py
@@ -1,5 +1,20 @@
+from functools import wraps
+
+
 def singlerulematcher():
+    register = {"strategy": None}
+
     def func(*args, **kwargs):
+        if register["strategy"]:
+            return register["strategy"](*args, **kwargs)
         raise NotImplementedError
+
+    def register_strategy():
+        def decorator(strategy_func):
+            register["strategy"] = strategy_func
+
+        return decorator
+
+    func.register_strategy = register_strategy
 
     return func
--- rulematcher/test_rulematcher.py
+++ rulematcher/test_rulematcher.py
@@ -7,3 +7,19 @@ def test_rule_matcher_no_rule_registered():
     add = singlerulematcher()
     with pytest.raises(NotImplementedError):
         add(1, 2)
+
+
+def test_rule_matcher_one_matching_strategy():
+    add = singlerulematcher()
+
+    @add.register_strategy()
+    def add_integers(a, b):
+        if type(a) == type(b) == int:
+            return a + b
+
+    assert add(1, 2) == 3
+
+
+def test_multiple_rulematchers_dont_interfere():
+    # TODO
+    ...
