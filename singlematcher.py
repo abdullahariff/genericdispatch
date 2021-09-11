fbe638e2c99b4ac6a7a69cd0e038c70352c1a8d9
Abdullah Ariff <24273084+abdullahariff@users.noreply.github.com>
2021-09-11 20:44:45 +0100

    Black

--- singlematcher.py
+++ singlematcher.py
@@ -1,40 +1,40 @@
 from functools import wraps
 
+
 def singlematcher(default_func):
-  """Single-dispatch matcher function decorator.
-  
-  Like functools.singledispatch, but matches identity instead of type.
-  Transforms a function into a generic function, which can have different
-  behaviours depending upon the identity of the first argument. The decorated
-  function acts as the default implementation, and additional
-  implementations can be registered using the .register() attribute of the
-  generic function.
+    """Single-dispatch matcher function decorator.
 
-  Any hashable object may be registered as a match.
-  """
-  registry = {}
-  
-  @wraps(default_func)
-  def wrapper(matching_arg, *args, **kwargs):
-    try:
-      matched_func = registry[matching_arg]
-    except KeyError:
-      matched_func = default_func
-    return matched_func(matching_arg, *args, **kwargs)
+    Like functools.singledispatch, but matches identity instead of type.
+    Transforms a function into a generic function, which can have different
+    behaviours depending upon the identity of the first argument. The decorated
+    function acts as the default implementation, and additional
+    implementations can be registered using the .register() attribute of the
+    generic function.
 
-  def register(match):
-    def decorator(func):
-      if match in registry:
-        raise ValueError(
-          f"Cannot register {func!r} to matcher '{match}', "
-          f"already registered to {registry[match]!r}"
-        )
-      registry[match] = func
-      return func
-    return decorator
+    Any hashable object may be registered as a match.
+    """
+    registry = {}
 
-  wrapper.register = register
-    
-  return wrapper
-    
-    
+    @wraps(default_func)
+    def wrapper(matching_arg, *args, **kwargs):
+        try:
+            matched_func = registry[matching_arg]
+        except KeyError:
+            matched_func = default_func
+        return matched_func(matching_arg, *args, **kwargs)
+
+    def register(match):
+        def decorator(func):
+            if match in registry:
+                raise ValueError(
+                    f"Cannot register {func!r} to matcher '{match}', "
+                    f"already registered to {registry[match]!r}"
+                )
+            registry[match] = func
+            return func
+
+        return decorator
+
+    wrapper.register = register
+
+    return wrapper
--- test_singlematcher.py
+++ test_singlematcher.py
@@ -2,109 +2,116 @@ import inspect
 import pytest
 from .singlematcher import singlematcher
 
-def test_calling_decorated_function_returns_default():
-  @singlematcher
-  def do_thing(match):
-    return f"{match} thing"
-  
-  assert do_thing("foo") == "foo thing"
-  
-def test_registering_a_matcher_but_no_match_found():
-  @singlematcher
-  def do_thing(match):
-    return "default match"
 
-  @do_thing.register("matchme!")
-  def _(match):
-    return "I match!"
-  
-  assert do_thing("foo") == "default match"
-  assert do_thing("doesnt match") == "default match"
+def test_calling_decorated_function_returns_default():
+    @singlematcher
+    def do_thing(match):
+        return f"{match} thing"
+
+    assert do_thing("foo") == "foo thing"
+
+
+def test_registering_a_matcher_but_no_match_found():
+    @singlematcher
+    def do_thing(match):
+        return "default match"
+
+    @do_thing.register("matchme!")
+    def _(match):
+        return "I match!"
+
+    assert do_thing("foo") == "default match"
+    assert do_thing("doesnt match") == "default match"
 
 
 def test_matching_a_registered_matcher():
-  @singlematcher
-  def do_thing(match):
-    return "default match"
+    @singlematcher
+    def do_thing(match):
+        return "default match"
 
-  @do_thing.register("matchme!")
-  def _(match):
-    return f"I match {match}"
-  
-  assert do_thing("foo") == "default match"
-  assert do_thing("matchme!") == "I match matchme!"
+    @do_thing.register("matchme!")
+    def _(match):
+        return f"I match {match}"
+
+    assert do_thing("foo") == "default match"
+    assert do_thing("matchme!") == "I match matchme!"
 
 
 def test_always_uses_default_matcher_docstring():
-  @singlematcher
-  def do_thing(match):
-    """
+    @singlematcher
+    def do_thing(match):
+        """
     This is my docstring.
     """
-    return "default match"
+        return "default match"
 
-  @do_thing.register("matchme!")
-  def _(match):
-    "This is some other docstring"
-    return "I match!" 
+    @do_thing.register("matchme!")
+    def _(match):
+        "This is some other docstring"
+        return "I match!"
 
-  assert inspect.getdoc(do_thing) == "This is my docstring."
+    assert inspect.getdoc(do_thing) == "This is my docstring."
 
 
 def test_registering_same_match_twice_fails():
-  @singlematcher
-  def do_thing(match):
-    return "default match"
+    @singlematcher
+    def do_thing(match):
+        return "default match"
 
-  @do_thing.register("matchme!")
-  def _(match):
-    return "I match!"
-
-  with pytest.raises(ValueError) as exc:
     @do_thing.register("matchme!")
     def _(match):
-      return "I also match"
-  
+        return "I match!"
+
+    with pytest.raises(ValueError) as exc:
+
+        @do_thing.register("matchme!")
+        def _(match):
+            return "I also match"
+
 
 def test_register_multiple_matches_to_single_function():
-  @singlematcher
-  def do_thing(match):
-    return "default match"
+    @singlematcher
+    def do_thing(match):
+        return "default match"
 
-  @do_thing.register("matchme!")
-  @do_thing.register("match again")
-  def _(match):
-    return f"I match {match}!"
+    @do_thing.register("matchme!")
+    @do_thing.register("match again")
+    def _(match):
+        return f"I match {match}!"
 
-  assert do_thing("matchme!") == "I match matchme!!"
-  assert do_thing("match again") == "I match match again!"
+    assert do_thing("matchme!") == "I match matchme!!"
+    assert do_thing("match again") == "I match match again!"
+
 
 def test_other_arguments_passed_through():
-  @singlematcher
-  def do_thing(_, something):
-    return f"default match {something}"
+    @singlematcher
+    def do_thing(_, something):
+        return f"default match {something}"
 
-  @do_thing.register("matchme!")
-  def _(_, something, keyword="myword"):
-    return f"I match {something} with {keyword}!"
+    @do_thing.register("matchme!")
+    def _(_, something, keyword="myword"):
+        return f"I match {something} with {keyword}!"
 
-  assert do_thing("nomatch", "thing") == "default match thing"
-  assert do_thing("matchme!", "other thing") == "I match other thing with myword!"
-  assert do_thing("matchme!", "other thing", keyword="password") == "I match other thing with password!"
+    assert do_thing("nomatch", "thing") == "default match thing"
+    assert do_thing("matchme!", "other thing") == "I match other thing with myword!"
+    assert (
+        do_thing("matchme!", "other thing", keyword="password")
+        == "I match other thing with password!"
+    )
 
 
 def test_with_other_types_of_hashable_objects():
-  @singlematcher
-  def do_thing(_):
-    raise NotImplementedError(f"No match found for {_}")
+    @singlematcher
+    def do_thing(_):
+        raise NotImplementedError(f"No match found for {_}")
 
-  @do_thing.register(56)
-  def _(_):
-    return "I'm an integer"
+    @do_thing.register(56)
+    def _(_):
+        return "I'm an integer"
 
-  @do_thing.register(("fat", 123))
-  def _(_):
-    return "I'm a tuple"
+    @do_thing.register(("fat", 123))
+    def _(_):
+        return "I'm a tuple"
 
-  assert do_thing(56) == "I'm an integer"
-  assert do_thing(("fat", 123)) == "I'm a tuple"
+    assert do_thing(56) == "I'm an integer"
+    assert do_thing(("fat", 123)) == "I'm a tuple"
